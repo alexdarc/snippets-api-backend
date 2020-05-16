e598901d7c352a9b0ae5d12b005aa11074b12eff
Alexander Kim <alex.dark.kim@gmail.com>
2020-05-16 23:52:03 +0300

    added mongodb driver

--- Infrastructure/Mappers/SnippetMapper.cs
+++ Infrastructure/Mappers/SnippetMapper.cs
@@ -3,10 +3,13 @@ namespace Infrastructure.Mappers
     using System.Collections.Generic;
     using Contracts.Models;
     using Contracts.Services;
+    using MongoDB.Driver;
 
     public class SnippetMapper
         : ISnippetMapper
     {
+        private readonly IMongoCollection<Snippet> snippetsCollection;
+
         public Snippet Get(
             string id)
         {
