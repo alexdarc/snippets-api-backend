c6a4672be0899a45328d8f342583f148d2f8705f
Alexander Kim <alex.dark.kim@gmail.com>
2020-05-20 21:32:12 +0300

    set max length

--- SnippetsApi/Features/Version1/Snippets/Actions/Update/UpdateRequestModel.cs
+++ SnippetsApi/Features/Version1/Snippets/Actions/Update/UpdateRequestModel.cs
@@ -5,9 +5,11 @@ namespace SnippetsApi.Features.Version1.Snippets.Actions.Update
     public class UpdateRequestModel
     {
         [Required]
+        [MaxLength(length: 1024)]
         public string Description { get; set; }
 
         [Required]
+        [MaxLength(length: 255)]
         public string Content { get; set; }
     }
 }
